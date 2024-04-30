d6e048b0ae1288e048a211bed6af969e5555fbe9
Emre Yesilyurt <yesilyurtemre@outlook.com.tr>
2024-05-01 01:40:28 +0300

    Initial Commit

---
+++ TravelGuide/TravelGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelGuideApp.swift
+//  TravelGuide
+//
+//  Created by Emre Yeşilyurt on 1.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
